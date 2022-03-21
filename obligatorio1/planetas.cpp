a3aea5cf5ae81ed5dcdb786840ebca39d73fc4b3
Camachoadri <camachoadri@correo.ugr.es>
2022-03-21 19:58:42 +0100

    clase lunes 21

--- obligatorio1/planetas.cpp
+++ obligatorio1/planetas.cpp
@@ -4,8 +4,32 @@ using namespace std;
 
 int main (void){
 
-    cout << "Hola mundo.";
+    // Vamos a declarar las variables que usaremos
+    int N;
 
+    //Damos valor a cuantos planetas vamos a tener inicialmente
+
+    N=32;
+
+    float h, t, t_f,  r[N], v[N], a[N], w[N], G, c, M_s;
+
+    //Damos valores al resto de variables e inciamos las necesarias
+    h=0.1;
+
+    // Tiempo para el que queremos que acabe
+    t_f=4;
+    //Ahora nombraremos las constantes del problema
+
+
+
+    //Comenzamos el buble
+
+    for (t=0; t <= t_f ; t=t+h)
+    {
+        
+    }
+    
+    
 
     return 0;
 }
