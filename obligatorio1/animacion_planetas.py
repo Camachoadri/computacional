729bc4d575af3ae6dbc3daba10803000dc0dcd5b
Camachoadri <camachoadri@correo.ugr.es>
2022-04-04 19:56:26 +0200

    final planetas

--- obligatorio1/animacion_planetas.py
+++ obligatorio1/animacion_planetas.py
@@ -52,15 +52,15 @@ file_in = "planets_data.dat" # Nombre del fichero de datos
 file_out = "estabilidad" # Nombre del fichero de salida (sin extensión)
 
 # Límites de los ejes X e Y
-x_min = -35
-x_max = 35
-y_min = -35 
-y_max = 35
+x_min = -70
+x_max = 70
+y_min = -70 
+y_max = 70
 
 interval = 10 # Tiempo entre fotogramas en milisegundos
 show_trail = True # Muestra la "estela" del planeta
 trail_width = 0.5 # Ancho de la estela
-save_to_file = True # False: muestra la animación por pantalla,
+save_to_file = False # False: muestra la animación por pantalla,
                      # True: la guarda en un fichero
 dpi = 150 # Calidad del vídeo de salida (dots per inch)
 
