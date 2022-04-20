87f7e3cf87d723f5a76fdc7e0e7b7bebc33cee1f
Camachoadri <camachoadri@correo.ugr.es>
2022-04-20 18:21:27 +0200

    mitad

--- obligatorio2/ising.cpp
+++ obligatorio2/ising.cpp
@@ -6,18 +6,23 @@
 
 using namespace std;
 
+#define N 64
+
+float magnetizacion (float s[][N]);
+float energia (float s[][N]);
+
 int main (void){
 
     float T;
-    int N=64, i, j,k,n,m,aux;
-    float s[N][N],p, DE, xi;
+    int i, j,k,n,m,aux,pmonte;
+    float s[N][N],p, DE, xi, mn,en,cn;
     ofstream fich;
 
     //Inicializamso las variables necesarias
 
-    T=0.000000001;
+    T=0.05;
 
- /*
+
     for ( i = 0; i < N; i++)
     {
         for ( j = 0; j < N; j++)
@@ -26,11 +31,11 @@ int main (void){
         }
         
     }
-*/
+
 
  //Si queremos que sean aleatorias   
 
-
+/*
     for ( i = 0; i < N; i++)
     {
         for ( j = 0; j < N; j++)
@@ -45,7 +50,7 @@ int main (void){
         
     }
     
-
+*/
   
     fich.open("ising_data.dat");
 
@@ -61,6 +66,7 @@ int main (void){
         //Escribamos la matriz en el fichero
 
         aux= (i%(N*N));
+        pmonte=0;
 
         if ((i%(N*N))==0)
         {
@@ -75,6 +81,7 @@ int main (void){
 
             }
                 fich << endl;
+            pmonte++;
         }
         
         
@@ -150,6 +157,67 @@ int main (void){
     
     fich.close();
 
+    mn=0;
+    en=0;
+    cn=0;
+
+    //Para el voluntario vamos a hacer la media
+
+    if ((pmonte%100)==0)
+    {
+        mn= mn + magnetizacion(s);
+    }
+    
+    mn= mn/(pmonte/100);
+
+
     return 0;
 
 }
+
+
+float magnetizacion (float s[][N]){
+
+
+    float m;
+    int i,j;
+    m=0;
+
+    for ( i = 0; i < N; i++)
+    {
+        for ( j = 0; j < N; j++)
+        {
+            m=m+s[i][j];
+        }
+        
+    }
+    
+    m=m/(N*N);
+
+    return m;
+}
+
+float energia (float s[][N]){
+
+    float E=0;
+    int i,j;
+
+    //Hacemos la suma teniendo en cuanta qu eno queremos que desborde la matriz
+
+    for ( i = 1; i < N-1; i++)
+    {
+        for ( j = 1; j < N-1; j++)
+        {
+            E=s[i][j]*(s[i][j+1]);
+        }
+        
+    }
+    
+
+
+
+
+
+    return E;
+}
+
