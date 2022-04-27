54e44adf8d3a68d5637a1f856a672ac3c882ae86
Camachoadri <camachoadri@correo.ugr.es>
2022-04-27 18:49:47 +0200

    final clase, no tengo ni idea de q estoy haciendo

--- obligatorio3/schrodinguer.cpp
+++ obligatorio3/schrodinguer.cpp
@@ -5,11 +5,21 @@
 
 
 using namespace std;
+#define N 32
 
 
 int main(void){
 
+    int i, n_ciclos, N;
+    float lambda, alpha[N], beta[N], xi[N][N], phi[N][N],s, v[N];
 
+    //Vamos ainiciar la matriz de la funcion de onda
+
+    for ( i = 0; i < N; i++)
+    {
+        
+    }
+    
 
 
 
