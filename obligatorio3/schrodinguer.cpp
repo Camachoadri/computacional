f2d6f34302d431f129b72cd2513eb12a8e40aba1
Camachoadri <camachoadri@correo.ugr.es>
2022-05-11 18:07:43 +0200

    final final

--- obligatorio3/schrodinguer.cpp
+++ obligatorio3/schrodinguer.cpp
@@ -17,11 +17,7 @@ int main(void){
 
     complex <double> phi[200], alpha[200], beta[200], xi[200], i;
     double s, k_0, V[200], lambda, norma;
-<<<<<<< HEAD
     ofstream fich, norm_fich;
-=======
-    ofstream fich, norm;
->>>>>>> a1fe74bb7a9ee7f1aa3591142ce0129467d7cf9a
     i= complex<double>(0.0,1.0); 
 
     //Vamos a iniciar la matriz de la funcion de onda y los parametros necesarios
@@ -62,11 +58,7 @@ int main(void){
     }
     
     fich.open("schrodinger_data.dat"); 
-<<<<<<< HEAD
     norm_fich.open("norma.dat");
-=======
-    norm.open("norm.dat");
->>>>>>> a1fe74bb7a9ee7f1aa3591142ce0129467d7cf9a
 
 
     for ( n = 0; n < npasos; n++)
@@ -105,18 +97,12 @@ int main(void){
       //  cout << phi[0];
         
         norm_fich << norma << endl;
-        
-        norm << norma << endl;
-        
+                
         fich << endl;
     }
     
     fich.close();
-<<<<<<< HEAD
     norm_fich.close();
-=======
-    norm.close();
->>>>>>> a1fe74bb7a9ee7f1aa3591142ce0129467d7cf9a
 
 
     return 0;
