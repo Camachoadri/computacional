1138479a53fbc948ca794734078836e6cdfdc52d
Camachoadri <camachoadri@correo.ugr.es>
2022-05-16 19:12:38 +0200

    mitad

--- obligatorio4/nave.cpp
+++ obligatorio4/nave.cpp
@@ -14,8 +14,9 @@ using namespace std;
 #define w 2.6617*pow(10,-6)
 #define R_t 6.378*pow(10,6)
 #define R_l 1.7374*pow(10,6)
-#define npasos 1000
-#define m 1000
+#define npasos 100000
+#define m 30000
+#define theta 0.3
 
 //Vamos a hacer una funcion para la p_phi
 
@@ -27,26 +28,41 @@ int main(void){
 
     //Comencemos con el algoritmo declarando las varibles necesarias, tanto contadores como posiciones de la nave y la luna
 
-    float lunap[2], coheter, cohetephi, p_r, p_phi, h=1., k[4][4];
+    float lunar, lunaphi, coheter, cohetephi, p_r, p_phi, h=1., k[4][4],v;
     int i,j;
+    ofstream fich;
 
     //Ahora inicializamos los parametro
 
-    lunap[0]=dtl;
-    lunap[1]=0.;
+    lunar=dtl;
+    lunaphi=0.;
     coheter=R_t;
-    cohetephi=0;
-    p_r=0;
-    p_phi=0;
+    cohetephi=0.;
+    v=60000;
 
-    //A continuacion emepzamos el ciclo
+    //Vamos a reescalar
+
+    lunar=lunar/(1.*dtl);
+    coheter=R_t/(1.*dtl);
+    v=v/(1.*dtl);
+    p_r=v*cos(theta);
+    p_phi=v*coheter*sin(theta);  
+
+    //A continuacion emepzamos el ciclo y abrimos el fichero
+
+    fich.open("planets_data.dat");
 
     for ( i = 0; i < npasos; i++)
     {
+        //Lo primero es escribir en el documento las posiciones    
+
+        
+        fich << coheter*cos(cohetephi) << ", " << coheter*sin(cohetephi) << endl << lunar*cos(w*i*h*1.) << ", " << lunar*sin(w*i*h*1.) << endl << endl;
+
         //Evaluamos los k donde el primer indice indica el superindice de k y el segundo indica de que coordenada
         //El primero es r el segundo es phi el tercero p_r y el cuarto p_phi
 
-        k[0][0]=h*p_r/m;
+        k[0][0]=h*p_r;
         k[0][1]=h*p_phi/(coheter*coheter);
         k[0][2]=h*ppunto_r(coheter, cohetephi, p_phi, i*h);
         k[0][3]=h*ppunto_phi(coheter, cohetephi, i*h);
@@ -66,11 +82,17 @@ int main(void){
         k[3][2]=h*ppunto_r(coheter+ k[2][2]/2, cohetephi + k[2][1]/2, p_phi+ k[2][3]/2 , i*h);
         k[3][3]=h*ppunto_phi(coheter+ k[2][0], cohetephi+k[2][1], i*h);
 
-        
+        //Ahora calcularemos los valores de y
+
+        coheter=coheter + (k[0][0]+2*k[1][0]+2*k[2][0]+k[3][0])/6;
+        cohetephi= cohetephi + (k[0][1]+2*k[1][1]+2*k[2][1]+k[3][1])/6;
+        p_r= p_r + (k[0][2]+2*k[1][2]+2*k[2][2]+k[3][2])/6;
+        p_phi= p_phi + (k[0][3]+2*k[1][3]+2*k[2][3]+k[3][3])/6;
+
     }
     
 
-
+    fich.close();
 
 
     return 0;
@@ -80,8 +102,8 @@ float ppunto_r(float coheter, float cohetephi, float p_phi, float t){
 
     float ppunto_r,DELTA, mu,rprima;
 
-    DELTA=G*M_t/(dtl*dtl*dtl);
-    mu=M_l/M_t;
+    DELTA=G*M_t/(1.*dtl*dtl*dtl);
+    mu=M_l/(1.*M_t);
     rprima=sqrt(1+coheter*coheter-2*coheter*cos(cohetephi-w*t));
 
     ppunto_r=p_phi*p_phi/(coheter*coheter*coheter)-DELTA*(1/(coheter*coheter)+mu/(rprima*rprima*rprima)*(coheter-cos(cohetephi-w*t)));
@@ -96,8 +118,8 @@ float ppunto_phi(float coheter, float cohetephi, float t){
 
     float ppunto_phi,DELTA, mu,rprima;
 
-    DELTA=G*M_t/(dtl*dtl*dtl);
-    mu=M_l/M_t;
+    DELTA=G*M_t/(1.*dtl*dtl*dtl);
+    mu=M_l/(1.*M_t);
     rprima=sqrt(1+coheter*coheter-2*coheter*cos(cohetephi-w*t));
 
     ppunto_phi=-DELTA*mu*coheter*sin(cohetephi-w*t)/(rprima*rprima*rprima);
